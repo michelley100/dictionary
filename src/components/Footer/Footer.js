cb5b5c53ad7c7b5a06d730181f912f049eb419c1
DN <nongphuddaya@gmail.com>
2021-10-16 14:11:39 +0530

    2nd commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <hr style={{ width: "90%", marginTop: 20 }} />
+      <span className="name">Made by DN</span>
+    </div>
+  );
+};
+
+export default Footer;
